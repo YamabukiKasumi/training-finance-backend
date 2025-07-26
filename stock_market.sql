8ead96ecd8497c9db01355d8cb0616cce3a5bbd9
wjy <2368549779@qq.com>
2025-07-26 19:39:52 +0800

    update stock_market.sql

--- stock_market.sql
+++ stock_market.sql
@@ -1,10 +1,11 @@
 USE finance_data;
 CREATE TABLE stock_market (
-    symbol VARCHAR(10),
-    stockType VARCHAR(50),
-    lastSalePrice DECIMAL(10,2),
-    netChange DECIMAL(10,2),
-    percentageChange DECIMAL(5,2),
-    deltaIndicator VARCHAR(10),
-    volume BIGINT
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    symbol VARCHAR(10) NOT NULL,			-- 股票代码
+    stockType VARCHAR(50) NOT NULL,			-- 股票类型
+    lastSalePrice DECIMAL(10,2) NOT NULL,	-- 最新成交价格，单位通常为美元
+    netChange DECIMAL(10,2) NOT NULL,		-- 相对前一交易日的净变动值
+    percentageChange DECIMAL(5,2) NOT NULL,	-- 相对前一交易日的涨跌幅（百分比）
+    deltaIndicator VARCHAR(10) NOT NULL,	-- 涨跌方向：可为 "up" 或 "down"
+    volume BIGINT							-- 当日累计成交量（股数）
 );
