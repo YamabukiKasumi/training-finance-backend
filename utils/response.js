8eae80a760f6ae9bdacb5d53496407fce136b3c3
Jasper <yamabuki1999@qq.com>
2025-07-28 09:55:55 +0800

    No comment

--- utils/response.js
+++ utils/response.js
@@ -1,11 +1,20 @@
-exports.successResponse = (data, msg = '获取成功') => ({
-  status: 'success',
-  msg,
-  data,
-});
+const { StatusCodes } = require('http');
 
-exports.errorResponse = (msg = '请求失败') => ({
-  status: 'error',
-  msg,
-  data: null,
-});
+function formatResponse({ code, message, data = null }) {
+  return {
+    code,
+    message,
+    data
+  };
+}
+
+function successResponse(data = null, message = '成功', code = StatusCodes.OK) {
+  return formatResponse({ code, message, data });
+}
+
+function errorResponse(message = '服务器错误', code = StatusCodes.INTERNAL_SERVER_ERROR, data = null) {
+  return formatResponse({ code, message, data });
+}
+
+exports.successResponse = successResponse;
+exports.errorResponse = errorResponse;
