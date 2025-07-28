2c62a7e0e87cb0ffa4a0cc288de64b13731163de
wwwjiayao <143187300+wwwjiayao@users.noreply.github.com>
2025-07-28 14:05:57 +0800

    update DB password

--- db/mysql.js
+++ db/mysql.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',     // 你的 MySQL 服务器地址
   user: 'root', // 你的 MySQL 用户名
-  password: '202507', // 你的 MySQL 密码
+  password: '123456', // 你的 MySQL 密码
   database: 'finance_data', // 你的数据库名
 });
 
