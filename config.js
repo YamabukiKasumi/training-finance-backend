f18f8b4794e9374701f4d1a3c72d146c5f7ce58e
Jasper <yamabuki1999@qq.com>
2025-07-30 10:20:30 +0800

    fix: task 8 - update the key

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ module.exports = {
     quoteEndpoint: '/quote', // 指数报价的相对路径
     ratingSnapshotEndpoint: '/ratings-snapshot', // *** 1. 新增评分端点 ***
     historicalPriceFullEndpoint: '/historical-price-eod/full', // *** 新增这个端点 ***
-    apiKey: 'mh1PytCioaQIwCWp2rQhQU2RmUR5Fw4I', // 替换为你的 FMP API Key
+    apiKey: 'i8PzpWD5VC1A8vDZZu7ZcBbE9HmlzPED', // 替换为你的 FMP API Key
   },
   // *** 新增获取指数信息的默认符号和间隔 ***
   indexConfig: {
